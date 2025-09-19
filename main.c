6c6de27df7f7e45d40089171b1bea9b36185e442
Commander Dream <chayakorn11062549@gmail.com>
2025-09-19 13:55:44 +0700

    main.c

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
+#define FILENAM "orders.csv"
+
+void display_orders(){
+    FILE *file = fopen(FILENAM, "r");
+    if (!file){
+        printf("Error: Cannot open file %s\n", FILENAM);
+        return;
+    }
+    char line[256];
+    while (fgets(line, sizeof(line), file)){
+        printf("%s", line);
+    }
+    fclose(file);
+}
+
+int main(){
+    printf("=== Order List ===\n");
+    display_orders();
+    return 0;
+}
